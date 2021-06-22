a0cf2f2274a4552c268bb0e0205f88dbc4abbb93
Michael O'Connor <oconnor97@aol.com>
2021-06-22 12:39:10 -0400

    editing the layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header/index';
 import About from './components/About/index';
+import Work from './components/Work/index'
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Work />
     </>
 
   );
